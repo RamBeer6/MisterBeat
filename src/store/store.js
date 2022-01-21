81640b9d14f7195c858b89e2f58102f6fa9693a7
nicole elezra <im.nicole.m@gmail.com>
2022-01-21 13:23:35 +0200

    store player

--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,13 @@
-import { createStore, applyMiddleware, combineReducers, compose } from 'redux'
-import thunk from 'redux-thunk'
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
+import thunk from 'redux-thunk';
 
-import { stationReducer } from './reducers/station.reducer'
+import { stationReducer } from './reducers/station.reducer';
+import { playerReducer } from './reducers/player.reducer';
 
 const rootReducer = combineReducers({
-    stationModule: stationReducer,
+  stationModule: stationReducer,
+  playerModule: playerReducer,
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-export const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
-);
+export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
