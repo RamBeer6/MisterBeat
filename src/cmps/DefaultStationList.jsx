25ed0a5f58e382c70c585e2790a7318910752548
nicole elezra <im.nicole.m@gmail.com>
2022-01-26 12:33:56 +0200

    css

--- src/cmps/DefaultStationList.jsx
+++ src/cmps/DefaultStationList.jsx
@@ -1,19 +1,23 @@
-import { StationPreview } from '../cmps/StationPreview'
+import { StationPreview } from '../cmps/StationPreview';
 
 export function DefaultStationList({ stations }) {
-    stations.sort((s1,s2) => s2.likedByUsers?.length - s1.likedByUsers?.length)
+  stations.sort((s1, s2) => s2.likedByUsers?.length - s1.likedByUsers?.length);
 
-    return(
-        <section className="default-station-list">
-            <div className="section-station">
-                <div className="section-head">
-                    <h2>Popular new releases</h2>
-                    <a>SEE ALL</a>
-                </div>
-                <div className="section-content">
-                    {stations.map(station => <StationPreview  key={station._id} station={station} />)}
-                </div>
-            </div>
-        </section>
-    )
+  return (
+    <section className='default-station-list'>
+      <div className='section-station'>
+        <div className='section-head'>
+          <h2>Popular new releases</h2>
+          <a>SEE ALL</a>
+        </div>
+        <div className='section-content'>
+          {stations.map((station) => (
+            <StationPreview key={station._id} station={station} />
+          ))}
+        </div>
+        <a className='switchLeft sliderButton'> &lt; </a>
+        <a className='switchRight sliderButton'> &gt; </a>
+      </div>
+    </section>
+  );
 }
